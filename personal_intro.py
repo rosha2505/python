a1df735230b8fd145fcf8ed0c59ac8a34e43d58b
Roshan Sharma <20242551009@siwscollege.edu.in>
2026-02-02 17:06:24 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,13 @@
+# Personal Introduction Program
+# This program collects user information and prints a friendly welcome message
+
+# Asking user questions
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("Which city do you live in? ")
+hobby = input("What is your favourite hobby? ")
+
+# Displaying a friendly welcome message using f-strings
+print(f"Welcome {name} 😊")
+print(f"you are living in {city}")
+print(f"You are {age} years old and loving {hobby}")
